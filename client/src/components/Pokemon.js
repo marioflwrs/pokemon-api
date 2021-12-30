8631956572db7c33ae141c2af5cf8e480fe438e5
wawamario <mariomuloz@gmail.com>
2021-12-30 04:14:11 -0500

    search input has been updated

--- client/src/components/Pokemon.js
+++ client/src/components/Pokemon.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
 import './pokemon.scss';
+import logo from './pokemon-logo.png';
+
+
 
 export default function Pokemon() {
 
   const [pokemon, setPokemon] = useState([]);
   const [searchPokemon, setSearchPokemon] = useState('');
+  const [pokemonType, setPokemonType] = useState('');
+  // const [cardColor, setCardColor] = useState('red');
+
+
   
 
 
@@ -23,38 +30,50 @@ export default function Pokemon() {
 
       .then((results) => {
         setPokemon(results.map((res) => res.data));
-      });
-
+        setPokemonType(results.map((res) => res.data.types[0].type.name));
+      })
 
   }, [])
 
-  console.log(searchPokemon);
-
   return (
-    <div className="pokemon-list-container">
-      <input 
-        type="text"
-        placeholder="Search Pokemon"
-        onChange={event => {setSearchPokemon(event.target.value)}}
-      />
+    <div className="pokemon-component-container">
+      
+      <img className="logo-style"src={logo} alt="" />
 
-        {pokemon.filter((val) => {
+      <div className="search-bar-container">
+        <input 
+          className="search-bar"
+          type="text"
+          placeholder="SEARCH POKEMON"
+          onChange={event => {setSearchPokemon(event.target.value)}}
+        />
+      </div> 
+      
+      <div className="pokemon-list-container">
+        
+        { pokemon.filter((val) => {
           if (searchPokemon === "") {
             //if input value is empty, return all pokemon.
             return val;
           }else if (val.name.toLowerCase().includes(searchPokemon.toLowerCase())){
-            //searches through list of pokemon containing string characters (all lowercase).
+            //searches through the list POKEAPI containing any string characters placed in the input field.
             return val;
           } //returning false to avoid .filter() warning.
            return false;
         }).map(p => ( 
-          <div className="pokemon-list-container" key={p.name}>
-            <div className="poke-card-container">
-              <img src={p.sprites.front_default} alt="" />
-              <h1>{p.name}</h1>
-            </div>
+
+          <div className="poke-card-container" key={p.name}>
+            <img src={p.sprites.front_default} alt="" />
+            <h1>{p.name}</h1>
+            <h1>{pokemonType[p.id-1]}</h1>
           </div>
         ))}
+
+        
+      </div>
+
     </div>
+
+    
   )
 }
